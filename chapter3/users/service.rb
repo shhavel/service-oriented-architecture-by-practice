3da50a5844a24427073cb769a643a03e4ea55002
Alex Avoyants <shhavel@gmail.com>
2015-01-03 16:12:45 +0200

    Basic dumb users device

---
+++ chapter3/users/service.rb
@@ -0,0 +1,18 @@
+require "sinatra/main"
+
+set :root, File.dirname(__FILE__)
+use Rack::CommonLogger, File.new(File.join(settings.root, 'log',
+  "#{settings.environment}.log"), 'a+').tap { |f| f.sync = true }
+
+get "/api/v1/users/me.json" do
+  content_type :json
+
+  case request.env['HTTP_AUTHORIZATION']
+  when nil then [403, '{"message":"Access Forbidden"}']
+  when "OAuth 562f9fdef2c4384e4e8d59e3a1bcb74fa0cff11a75fb9f130c9f7a146a003dcf"
+    then '{"user":{"type":"AdminUser"}}'
+  when "OAuth b259ca1339e168b8295287648271acc94a9b3991c608a3217fecc25f369aaa86"
+    then '{"user":{"type":"RegularUser"}}'
+  else [401, '{"message":"Invalid or expired token"}']
+  end
+end
