14632ef12354377b72fbd1c0a3256c9bc07d3328
Alex Avoyants <shhavel@gmail.com>
2015-01-06 08:58:48 +0200

    Basic implementation of logger in Divisor service

--- chapter2/divisor/service.rb
+++ chapter2/divisor/service.rb
@@ -1,6 +1,14 @@
 require "sinatra/main"
+require 'logger'
+
+set :root, File.dirname(__FILE__)
+use Rack::CommonLogger, File.new(File.join(settings.root, 'log',
+  "#{settings.environment}.log"), 'a+').tap { |f| f.sync = true }
+set :raise_errors, false
 
 get "/api/v1/ratio/:a/:b" do
   content_type :txt
-  "#{params[:a].to_f / params[:b].to_f}"
+  "#{params[:a].to_i / params[:b].to_i}"
 end
+
+error { [500, "An internal server error occurred. Please try again later."] }
--- chapter2/divisor/spec/service_spec.rb
+++ chapter2/divisor/spec/service_spec.rb
@@ -1,3 +1,4 @@
+ENV['RACK_ENV'] = 'test'
 require_relative "../service"
 require "rack/test"
 
@@ -11,10 +12,16 @@ end
 
 describe "Divisor Service" do
   describe "GET /api/v1/ratio/:a/:b" do
-    it "calculates the ratio of two numbers and returns decimal representation of the ratio" do
-      get "/api/v1/ratio/3/4"
+    it "computes the result of integer division of two integers" do
+      get "/api/v1/ratio/23/4"
       expect(last_response).to be_ok
-      expect(last_response.body).to eq "0.75"
+      expect(last_response.body).to eq "5"
+    end
+
+    it "handles unexpected errors" do
+      get "/api/v1/ratio/1/0"
+      expect(last_response.status).to eq 500
+      expect(last_response.body).to eq "An internal server error occurred. Please try again later."
     end
   end
 end
