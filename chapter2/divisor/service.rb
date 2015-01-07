d6342699ce6ea26353a7f34d7fada8c5f24ede0e
Alex Avoyants <shhavel@gmail.com>
2015-01-07 10:28:45 +0200

    Custom logging in request scope

--- chapter2/divisor/service.rb
+++ chapter2/divisor/service.rb
@@ -1,14 +1,22 @@
 require "sinatra/main"
-require 'logger'
+require "logger"
 
+set :raise_errors, false # in production and dev mode it is false, so you probably do not need it
 set :root, File.dirname(__FILE__)
-use Rack::CommonLogger, File.new(File.join(settings.root, 'log',
-  "#{settings.environment}.log"), 'a+').tap { |f| f.sync = true }
-set :raise_errors, false
+log_file = File.new(File.join(settings.root, 'log', "#{settings.environment}.log"), 'a+')
+log_file.sync = true
+use Rack::CommonLogger, log_file
+logger = Logger.new(log_file)
+logger.formatter = ->(severity, time, progname, msg) { "#{msg}\n" }
+
+before do
+  env['rack.logger'] = logger
+  content_type :txt
+end
 
 get "/api/v1/ratio/:a/:b" do
-  content_type :txt
+  logger.info "compute the result of integer division #{params[:a]} / #{params[:b]}"
   "#{params[:a].to_i / params[:b].to_i}"
 end
 
-error { [500, "An internal server error occurred. Please try again later."] }
+error { "An internal server error occurred. Please try again later." }
