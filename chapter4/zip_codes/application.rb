33435d11a641a2f0ca381f2c78c010cbaa0291dc
Alex Avoyants <shhavel@gmail.com>
2015-01-10 12:13:50 +0200

    Zip Codes service blueprint

---
+++ chapter4/zip_codes/application.rb
@@ -0,0 +1,15 @@
+require 'bundler/setup'
+Bundler.require :default, (ENV['RACK_ENV'] || :development).to_sym
+puts "Loaded #{Sinatra::Application.environment} environment"
+
+set :root, File.dirname(__FILE__)
+use Rack::CommonLogger, File.new(File.join(settings.root, 'log',
+  "#{settings.environment}.log"), 'a+').tap { |f| f.sync = true }
+
+require "sinatra/activerecord"
+Dir[File.join(settings.root, "app/models/*.rb")].each { |f| autoload File.basename(f, '.rb').classify.to_sym, f }
+Dir[File.join(settings.root, "app/controllers/*.rb")].each { |f| require f }
+
+before do
+  content_type :json
+end
