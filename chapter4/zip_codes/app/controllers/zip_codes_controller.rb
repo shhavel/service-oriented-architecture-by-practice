da3ad2d923fa4b00665698629149475140457867
Alex Avoyants <shhavel@gmail.com>
2015-01-18 11:51:31 +0200

    ZipCodes: Update record route

--- chapter4/zip_codes/app/controllers/zip_codes_controller.rb
+++ chapter4/zip_codes/app/controllers/zip_codes_controller.rb
@@ -10,3 +10,11 @@ get "/api/v1/zip_codes/:zip.json" do
   zip_code = ZipCode.find_by_zip!(params[:zip])
   zip_code.to_json
 end
+
+put "/api/v1/zip_codes/:id.json" do
+  param :id, Integer, max: 2147483647 # 0b111111111111111111111111111111
+  param :zip_code, Hash
+  zip_code = ZipCode.find(params[:id])
+  zip_code.update_attributes!(params[:zip_code])
+  zip_code.to_json
+end
--- chapter4/zip_codes/spec/acceptance/zip_codes_spec.rb
+++ chapter4/zip_codes/spec/acceptance/zip_codes_spec.rb
@@ -67,4 +67,50 @@ resource 'ZipCode' do
       expect(json_response[:errors][:zip]).to eq 'Parameter must match format (?-mix:\A\d{5}(?:-\d{4})?\Z)'
     end
   end
+
+  put "/api/v1/zip_codes/:id.json" do
+    header "Content-Type", "application/json"
+
+    parameter :id, "Zip", required: true
+    parameter :street_name, "Street name", scope: :zip_code
+    parameter :building_number, "Building number", scope: :zip_code
+    parameter :city, "City", scope: :zip_code
+    parameter :state, "State", scope: :zip_code
+    let(:raw_post) { params.to_json }
+
+    let(:zip_code) { create(:zip_code) }
+    let(:valid_attributes) { attributes_for(:zip_code) }
+
+    example "Update Zip Code" do
+      do_request(id: zip_code.id, zip_code: valid_attributes)
+      json_response = JSON.parse(response_body, symbolize_names: true)
+
+      expect(status).to eq 200
+      expect(json_response[:zip_code].values_at(:zip, :street_name, :building_number, :city, :state)).to eq(
+        valid_attributes.values_at(:zip, :street_name, :building_number, :city, :state))
+      expect(zip_code.reload.attributes.values_at(*valid_attributes.keys.map(&:to_s))).to eq valid_attributes.values
+    end
+
+    example "Update Zip Code that does not exist", document: nil do
+      do_request(id: 800)
+
+      expect(status).to eq 404
+      expect(response_body).to eq '{"message":"Record not found"}'
+    end
+
+    example "Read Zip Code provide to big ID number", document: nil do
+      do_request(id: 3000000000, zip_code: valid_attributes)
+      json_response = JSON.parse(response_body, symbolize_names: true)
+
+      expect(status).to eq 400
+      expect(json_response[:message]).to eq 'Invalid Parameter: id'
+      expect(json_response[:errors][:id]).to eq 'Parameter cannot be greater than 2147483647'
+    end
+
+    example "Read Zip Code provide not Hash zip_code params", document: nil do
+      do_request(id: zip_code.id, zip_code: "STRING")
+
+      expect(status).to eq 200
+    end
+  end
 end
